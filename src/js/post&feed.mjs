368a446c31f700e02f093f3f9d79b782aec91e9e
ArneBHuset <arne_hustveit@hotmail.com>
2023-11-20 21:28:44 +0100

    post$feed work

--- src/js/post&feed.mjs
+++ src/js/post&feed.mjs
@@ -1,1 +1,10 @@
 console.log("Hello world");
+
+function dynamicSidebar() {
+     var footerHeight = document.querySelector("footer").offsetHeight;
+     var sidebar = document.querySelector(".mobile-sidebar");
+     sidebar.style.height = `calc(100vh - ${footerHeight}px)`;
+     console.log("Footer height is:", footerHeight, "px");
+}
+
+dynamicSidebar();
