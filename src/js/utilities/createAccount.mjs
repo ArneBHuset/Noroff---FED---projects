c6824ecc6586946649b5a3e8a7ca48d56f1659fe
ArneBHuset <arne_hustveit@hotmail.com>
2023-11-26 22:00:33 +0100

    Figuring out javascript for form control

--- src/js/utilities/createAccount.mjs
+++ src/js/utilities/createAccount.mjs
@@ -1,5 +1,24 @@
-function createAccount() {
-  console.log("Start of create account function");
+import { validatedFormData } from "./forms.mjs";
+
+let createAccountFormData = {};
+
+function handleFormData(data) {
+  createAccountFormData = data;
+  console.log("createAccount.mjs", createAccountFormData);
+}
+
+validatedFormData(handleFormData);
+
+const API_BASE_URL = "https://nf-api.onrender.com";
+
+async function createAccount() {
+  console.log("Beginning of createAccount function:");
+
+  try {
+    //
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export { createAccount };
--- src/js/utilities/forms.mjs
+++ src/js/utilities/forms.mjs
@@ -1,5 +1,7 @@
-// Bootstrap default JS for form control
+// Default bootstrap validation is used and expanded to include password confirmation, confirmation of email
+// Containting neseccary values and creating a Json object of the data.
 "use strict";
+
 function validateBootstrapForms() {
   const forms = document.querySelectorAll(".needs-validation");
 
@@ -7,16 +9,55 @@ function validateBootstrapForms() {
     form.addEventListener(
       "submit",
       (event) => {
+        // NB!!!! prevent defualt for testing in console only, will stop form submisson
+        event.preventDefault();
+        // NB!!!! prevent defualt for testing in console only, will stop form submisson
+        const emailInput = document.getElementById("signUpEmail");
+        const chosenEmail = emailInput.value;
+        const isNoroffEmail = /@(stud\.noroff\.no|noroff\.no)$/.test(
+          chosenEmail
+        );
+        if (!isNoroffEmail) {
+          emailInput.setCustomValidity(
+            "Email must contain stud.noroff.no or noroff.no"
+          );
+        } else {
+          emailInput.setCustomValidity("");
+        }
+
+        const password = document.getElementById("signUpPassword").value;
+        const confirmPassword = document.getElementById(
+          "signUpConfirmPassword"
+        ).value;
+        if (password !== confirmPassword) {
+          document
+            .getElementById("signUpConfirmPassword")
+            .setCustomValidity("Passwords do not match.");
+        } else {
+          document
+            .getElementById("signUpConfirmPassword")
+            .setCustomValidity("");
+        }
+
         if (!form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
-        }
+          form.classList.add("was-validated");
+        } else {
+          form.classList.add("was-validated");
 
-        form.classList.add("was-validated");
+          const userName = document.getElementById("signUpUserName").value;
+          const userData = { userName, email: chosenEmail, password };
+          const userJSON = JSON.stringify(userData);
+          validatedFormData(userData);
+          console.log("Forms.mjs:", userJSON);
+        }
       },
       false
     );
   });
 }
-
+function validatedFormData(data) {}
 export { validateBootstrapForms };
+
+export { validatedFormData };
